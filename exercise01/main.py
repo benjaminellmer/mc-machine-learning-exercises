75b4b138bcb02240257d8a11ddc5f3bfac450286
benjaminellmer <benjamin.ellmer@yahoo.com>
2023-10-17 19:50:19 +0200

    add interpretations

--- exercise01/main.py
+++ exercise01/main.py
@@ -39,5 +39,22 @@ print(df.dtypes)
 print(df[0].value_counts())
 
 # 2.
+# 2.1.
 plot_gesture_per_participant("right")
-plot_gesture_for_participant("right", 0)
+# Yes we can see the trend that the gesture looks very similar for each participant.
+# The gesture consists of one huge peak that has a different breadth depending on the speed in which the gesture was
+# executed e.g. Participant 4 executed the gesture very fast and the gesture of participant 8 was the slowest one.
+
+# Yes we can see that each participant has a different delay, speed and strength while the pattern looks similar
+# for all of them.
+
+# 2.2.
+plot_gesture_for_participant("up", 0)
+plot_gesture_for_participant("up", 1)
+# Yes we can see the trend, that each sample of the participant has a very similar pattern.
+# Especially we can see that each try of the participant had a very similar delay, speed and strength.
+
+# Yes we can see, that the previous assumption does not work for both participants.
+# We can see that participant 1 had one (outlier) sample where the delay was very high, which could be the result
+# of a distraction during the recording.
+
